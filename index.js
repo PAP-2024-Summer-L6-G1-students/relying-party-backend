a457a36e53e40a5712464705d3c1da6c265769d1
timothy <timothy_hoangu@outlook.com>
2024-08-12 15:49:44 -0700

    Created routes express routes to read all and post events

--- index.js
+++ index.js
@@ -6,13 +6,43 @@ const Event = require('./models/Event'); // import for events model
 const app = express()
 const port = process.env.PORT || 3002
 
+// ------------ Routes ------------
+
+// TEST ROUTE
 app.get('/', (req, res) => {
   res.send('Hello World!')
 })
 
+// TEST ROUTE
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
 })
+
+// ROUTE 1 - GET ALL EVENTS
+app.get('/events', async (req, res) => {
+  try {
+    const events = await Event.readAllEvents();
+    res.status(200).json(events);
+  } catch (error) {
+    console.error(error);
+  }
+});
+
+// ROUTE 2 - CREATE EVENTS 
+app.post('/events', async (req, res) => {
+  try {
+    const eventData = req.body; // Get event data from the request body
+    const newEvent = await Event.createNew(eventData); // Create the new event
+    if (newEvent._id !== -1) {
+      res.status(201).json(newEvent); // EVENT CREATED SUCCESSFUL
+    } else {
+      res.status(400).json({ message: 'Failed to create event' }); // EVENT COULD NOT BE CREATED
+    }
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ message: 'Failed to create event' });
+  }
+});
 
 //* ********************* Launching the server **************** */
 const start = async () => {
